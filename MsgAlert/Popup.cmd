60c80c03c4877f7a6820b3c21220320d45f1a37f
Alex Wong <alw1746@users.noreply.github.com>
2021-01-23 16:06:27 +1100

    Add files via upload

--- MsgAlert/Popup.cmd
+++ MsgAlert/Popup.cmd
@@ -1,1 +1,11 @@
-
+@echo off
+::Pop up a msg alert box with a message and sound an alert.
+pushd "%cd%"
+cd "%~dp0"
+if [%1] == [] (
+  msg %username% "Alert message not provided."
+) else (
+  msg %username% %1
+) 
+cmdmp3.exe 006.wav
+popd
